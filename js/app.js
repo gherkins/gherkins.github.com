f44e4c4e15bbaf4c02878e281acf90019b6ac357
Max <maxgirkens@gmail.com>
2013-12-19 12:56:21 +0100

    undesign

---
+++ js/app.js
@@ -0,0 +1,111 @@
+var logo = [
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [1, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [1, 1, 0, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [1, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [1, 1, 0, 1, 0, 1, 0, 1, 1, 0, 0, 0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 0, 1, 0, 1, 1, 1, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [1, 1, 1, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 0, 1, 1, 1, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 0, 1, 0, 1, 1, 1, 1, 0, 1, 1, 0, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 0, 1, 1, 0, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 1, 0, 1, 1, 1, 1, 0, 1, 1, 0, 1, 0, 1, 0, 1, 1, 0, 0, 0, 1, 1, 0, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 0, 0, 0, 1, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 0, 1, 1, 0, 1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+];
+
+var work = [
+    {
+        date: "10/2013",
+        name: "Weihnachtskarten Designer",
+        desc: "design tool for christmas cards",
+        tech: "Silex, HTML5, JS",
+        link: "http://weihnachtskarten-designer.net/"
+    },
+    {
+        date: "7/2013",
+        name: "Eco Weihnachtskarten",
+        desc: "online shop for christmas cards",
+        tech: "Silverstripe, PHP, compass css",
+        link: "http://eco-weihnachtskarten.de"
+    },
+    {
+        date: "4/2013",
+        name: "MPC-Chord-Finder",
+        desc: "drumcomputer harmonics finder",
+        tech: "idea, CSS3, jQuery, music.js",
+        link: "http://web-development.cc/mpc-chordfinder/"
+    },
+    {
+        date: "4/2013",
+        name: "mashcloud.net",
+        desc: "collaborative web audio mashup",
+        tech: "idea, node.js. socket.io, backbone.js",
+        link: "http://mashcloud.net/"
+    }
+];
+
+
+$(function () {
+    $('#term').terminal(function (command, term) {
+
+            switch (command) {
+                case 'contact':
+                    term.echo("\n" +
+                        "email: maxgirkens@gmail.com\n" +
+                        "blog: http://nerdpress.org/author/max-girkens/\n" +
+                        "github: https://github.com/gherkins " +
+                        "\n");
+                    break;
+                case 'cv':
+                    term.echo("\n" +
+                        "CV: http://careers.stackoverflow.com/gherkins" +
+                        "\n");
+                    break;
+                case 'work':
+
+                    var output = "\n";
+                    $.each(work, function () {
+                        output += this.name + " - " + this.desc + " " + this.date + "\n"
+                        output += this.tech + "\n";
+                        output += this.link + "\n\n";
+                    });
+
+                    output += "...more coming soon\n\n";
+
+                    term.echo(output);
+                    break;
+                default:
+                    term.echo(command + ': command not found');
+                    break;
+            }
+
+        },
+        {
+            prompt: '>',
+            clear : false,
+            greetings: function () {
+
+                var output = 'Max Girkens\n';
+
+                $.each(logo, function (key, row) {
+                    $.each(row, function (key, char) {
+                        switch (char) {
+                            case 0:
+                                output += "[[;;#000]  ]"
+                                break;
+                            default:
+                                output += "[[;;#1ba9fc]  ]";
+                                break;
+                        }
+                    });
+                    output += "\n";
+                });
+
+                output += "\n" +
+                    "welcome to my homepage\n" +
+                    "commands: work, cv, contact\n";
+
+                return output;
+            }
+        });
+});
