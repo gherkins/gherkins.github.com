4ead13f99c3f3f2f4b6d7c0a2cd0339f4a7bbf46
Max <maxgirkens@gmail.com>
2013-12-23 13:09:22 +0100

    font preloading

--- js/app.js
+++ js/app.js
@@ -43,7 +43,22 @@ function greetings() {
 }
 
 
-$(function () {
+WebFontConfig = {
+    google: { families: [ 'Inconsolata::latin' ] },
+    active: init,
+    inactive: init
+};
+(function () {
+    var wf = document.createElement('script');
+    wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
+        '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+    wf.type = 'text/javascript';
+    wf.async = 'true';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(wf, s);
+})();
+
+function init() {
     $('#term').terminal(
 
         function (command, term) {
@@ -121,7 +136,7 @@ $(function () {
                 callback(['email', 'github', 'blog', 'twitter', 'cv', 'recentwork', 'clear']);
             }
         });
-});
+}
 
 var _gaq = _gaq || [];
 _gaq.push(['_setAccount', 'UA-38888541-4']);
