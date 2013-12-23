36825ee8c492e1324ef142026f1160e3161f531e
Max <maxgirkens@gmail.com>
2013-12-23 16:18:44 +0100

    lowercase command

--- js/app.js
+++ js/app.js
@@ -63,6 +63,8 @@ function init() {
 
         function (command, term) {
 
+            command = command.toLowerCase();
+
             //log
             if (command.trim() != '') {
                 try {
